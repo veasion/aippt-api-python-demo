1638f6406775fa36353ac7fbacb7bd056112cd43
veasion <veasion@qq.com>
2024-08-21 16:31:27 +0800

    modify

--- http_utils.py
+++ http_utils.py
@@ -5,7 +5,7 @@ def post_sse(url: str, headers: any, body: str, consumer, to_json=False) -> requ
     if not headers:
         headers = {}
     headers['Content-Type'] = 'application/json'
-    response = requests.post(url, headers=headers, data=body, stream=True)
+    response = requests.post(url, headers=headers, data=body, stream=True, verify=False)
     if response.status_code == 200:
         for line in response.iter_lines():
             str = line.decode('utf-8')
@@ -24,10 +24,10 @@ def post_json(url: str, headers: any, body: str) -> requests.Response:
     if not headers:
         headers = {}
     headers['Content-Type'] = 'application/json'
-    return requests.post(url, headers=headers, data=body)
+    return requests.post(url, headers=headers, data=body, verify=False)
 
 
 def download(url: str, save_path: str):
-    file = requests.get(url)
+    file = requests.get(url, verify=False)
     with open(save_path, 'wb') as wstream:
         wstream.write(file.content)
